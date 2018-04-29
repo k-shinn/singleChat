ed5cbe20a50b16d6dfd9a4568e0da8e90ec6bb11
k_shinn <k.shinn0721@gmail.com>
2018-04-30 00:05:17 +0900

    add timeline layout

--- app/src/main/java/com/tunnel/assignment/onepersonchat/MainActivity.kt
+++ app/src/main/java/com/tunnel/assignment/onepersonchat/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.tunnel.assignment.onepersonchat
 
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.tunnel.assignment.onepersonchat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.button.setOnClickListener({ view ->
+            val intent = Intent(view.context, TimeLineActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            view.context.startActivity(intent)
+        })
     }
 }
